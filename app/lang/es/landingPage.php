6816fdad7a640574367986ed77a107859d2524fa
gerson22 <gerson.isaias.rivera@gmail.com>
2016-08-02 20:54:07 -0500

    Landingpage Lang

---
+++ app/lang/es/landingPage.php
@@ -0,0 +1,15 @@
+<?php
+ return array(
+
+    'eslogan' =>'¡Que tu curiosidad no tenga límites!',
+    'menu'=>array(
+        'home' => 'Inicio',
+        'whatIs' => '¿Qué es Curiosity?',
+        'paymentOption' => 'Forma de Pago',
+        'howfunction' => '¿Cómo funciona?',
+        'video' => 'Video',
+        'createAccount' => 'Registrarse',
+        'logIn' => 'Iniciar Sesión'
+    )
+
+ );
